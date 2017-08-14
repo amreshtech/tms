e4dae31a158234ba4653b7127f22cd8485cdaa39
amreshtech <amreshtech@gmail.com>
2017-08-15 02:18:38 +0530

    misx

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output } from '@angular/core';
 import { Task } from 'app/tasks.model';
+import { LoginService } from 'app/login/login.service';
 declare var $: any;
 
 @Component({
@@ -9,16 +10,34 @@ declare var $: any;
 })
 export class HomeComponent implements OnInit {
   tasks: Array<Task> = [];
+  assignedToTasks: Array<Task> = [];
+  assignedByTasks: Array<Task> = [];
+  currentUser: string;
 
-  constructor() {
+  constructor(public loginService: LoginService) {
     if (!localStorage.getItem('tasks')) {
       localStorage.setItem('tasks', JSON.stringify(this.tasks));
     } else {
       this.tasks = JSON.parse(localStorage.getItem('tasks'));
     }
+    this.currentUser = loginService.getUser();
+    this.filterTasks();
   }
 
   ngOnInit() {
+  }
+
+  filterTasks(): void {
+    if (localStorage.getItem('tasks')) {
+      for (const i of this.tasks) {
+        if (i.assignedTo === this.currentUser) {
+          this.assignedToTasks.push(i);
+        }
+        if (i.assignedBy === this.currentUser) {
+          this.assignedByTasks.push(i);
+        }
+      }
+    }
   }
 
   showModal(): boolean {
